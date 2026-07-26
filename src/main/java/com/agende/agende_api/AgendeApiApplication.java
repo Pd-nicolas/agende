3f9e0538495754830058e7e6d8501d102f37a9c0
Pedro Nicolas <pedro.nicolas@cinbesa.com.br>
2026-07-25 22:04:24 -0300

    first commit

---
+++ src/main/java/com/agende/agende_api/AgendeApiApplication.java
@@ -0,0 +1,13 @@
+package com.agende.agende_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendeApiApplication.class, args);
+	}
+
+}
